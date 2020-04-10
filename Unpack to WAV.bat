90e3333f596acf62497a61aeedd4a81b0c5b0d59
Dr_Newbie <tf2baidonation@gmail.com>
2020-04-11 00:38:46 +0800

    small change for DOOM Eternal

---
+++ Unpack to WAV.bat
@@ -0,0 +1,62 @@
+FOR %%a IN ("Game Files\*.PCK") DO ("Tools\quickbms.exe" "Tools\wavescan.bms" "Game Files" "Tools\Decoding")
+FOR %%b IN ("Game Files\*.BNK") DO ("Tools\bnkextr.exe" "%%b" & MOVE *.wav "Tools\Decoding")
+FOR %%c IN (Tools\Decoding\*.WAV) DO ("Tools\vgmstream\test.exe" -o "%%c".WAV "%%c" & DEL "%%c" & MOVE "%%c".WAV "WAV")
+
+@echo off
+
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo    ((  '####@@!!$$    ))
+echo        `#####@@!$$`  ))
+echo     ((  '####@!!$:
+echo    ((  ,####@!!$:   ))
+echo        .###@!!$:
+echo        `##@@!$:
+echo         `#@!!$
+echo   !@#    `#@!$:       @#$
+echo    #$     `#@!$:       !@!
+echo             '@!$:
+echo         '`\   '!$: /`'
+echo            '\  '!: /'
+echo              '\ : /'
+echo   -.'-/\\\-.'//.-'/:'\.'-.JrS'.'-=_\\
+echo ' -.'-.\\'-.'//.-'.`-.'_\\-.'.-\'.-//
+echo      Watch out, it's the tornado!
+                                                                 
+echo.
+echo -------------------------------------------------------------
+
+echo Unpack finished! Files should be in the 'OGG' folder
+
+echo -------------------------------------------------------------
+echo.
+
+:choice
+
+set /P c=Should I delete BNKs and PCKs from the 'Game Files' folder [Y/N]?
+if /I "%c%" EQU "Y" goto :yes
+if /I "%c%" EQU "N" goto :hella_no
+goto :choice
+
+:yes
+
+FOR %%e IN ("Game Files\*.PCK") DO (DEL "%%e")
+FOR %%f IN ("Game Files\*.BNK") DO (DEL "%%f")
+
+echo Files deleted, enjoy your unpacked audio! -/u/Vextil ;)
+pause 
+exit
+
+:hella_no
+
+echo BNKs and PCKs kept, enjoy your unpacked audio! -/u/Vextil ;)
+
+pause 
+exit
+
+pause
